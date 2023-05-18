cf3bf54b1195640f6192a2f67cb29656041bea7a
BAI15 <15442866802qq.com>
2023-05-18 19:16:21 +0800

    新增了视频模块，并合并了两个模块

--- ruoyi-inform/src/test/java/com/ruoyi/inform/RuoyiInformApplicationTests.java
+++ ruoyi-inform/src/test/java/com/ruoyi/inform/RuoyiInformApplicationTests.java
@@ -1,13 +1,13 @@
-package com.ruoyi.inform;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class RuoyiInformApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.ruoyi.inform;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class RuoyiInformApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
